e71a75a38278c7eb758d16ebef04b89d41ed215b
Donald Ballowe <dballowe7912@gmail.com>
2022-07-21 18:44:52 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <>
-    App
-    </>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
